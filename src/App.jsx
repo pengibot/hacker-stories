724f7d95644a030f92322d2f369351415a81faf4
pengibot <pengibot@gmail.com>
2025-07-29 16:08:15 +0100

    changed functions to lambda types

--- src/App.jsx
+++ src/App.jsx
@@ -20,34 +20,37 @@ const list = [
   },
 ];
 
-function App() {
+const App = () => (
+    <div>
+      <h1>My Hacker Stories</h1>
+      <Search />
+      <hr />
+      <List />
+    </div>
+);
 
-  return (
-      <div>
-        <h1>My Hacker Stories</h1>
-        
-        <label htmlFor='search'>Search: </label>
-        <input id='search' type='text' />
+const Search = () => (
+  <div>
+    <label htmlFor='search'>Search: </label>
+    <input id='search' type='text' />
+  </div>
+);
 
-        <hr />
-
-        <ul>
-          { list.map(function(item) {
-            return (
-              <li key={item.objectID}>
-                <span>
-                  <a href={item.url}>{item.title}</a>
-                </span>
-                <span> {item.author}</span>
-                <span> {item.num_comments}</span>
-                <span> {item.points}</span>
-              </li>
-            )
-          })}
-        </ul>
-
-      </div>
-  )
-}
+const List = () => (
+  <ul>
+    { 
+      list.map((item) => (
+        <li key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span> {item.author}</span>
+          <span> {item.num_comments}</span>
+          <span> {item.points}</span>
+        </li>
+      ))
+    }
+  </ul>
+);
 
 export default App
