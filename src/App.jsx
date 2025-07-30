80c9e1121fea68b63a97d8b5fb39d3bcb0de32c4
pengibot <pengibot@gmail.com>
2025-07-30 12:28:46 +0100

    Created an Item component and passed down values using props. Finally, created a useState variable to hold the state

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,76 @@
 import './App.css'
+import * as React from 'react';
 
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
 
-const App = () => (
+
+const App = () => {
+
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+  
+  return (
     <div>
       <h1>My Hacker Stories</h1>
       <Search />
       <hr />
-      <List />
+      <List list={stories} />
     </div>
+  )
+};
+
+const Search = () => {
+
+  const [searchTerm, setSearchTerm] = React.useState('');
+
+  const handleChange = (event) => {
+    setSearchTerm(event.target.value);
+  }
+  
+  return (
+    <div>
+      <label htmlFor='search'>Search: </label>
+      <input id='search' type='text' onChange={handleChange} onBlur={handleChange} />
+    
+      <p>
+        Searching for <strong>{searchTerm}</strong>
+      </p>
+    </div>
+  )
+};
+
+const Item = (props) => (
+  <li key={props.item.objectID}>
+    <span>
+      <a href={props.item.url}>{props.item.title}</a>
+    </span>
+    <span> {props.item.author}</span>
+    <span> {props.item.num_comments}</span>
+    <span> {props.item.points}</span>
+  </li>
 );
 
-const Search = () => (
-  <div>
-    <label htmlFor='search'>Search: </label>
-    <input id='search' type='text' />
-  </div>
-);
-
-const List = () => (
+const List = (props) => (
   <ul>
     { 
-      list.map((item) => (
-        <li key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span> {item.author}</span>
-          <span> {item.num_comments}</span>
-          <span> {item.points}</span>
-        </li>
+      props.list.map((item) => (
+        <Item key={item.objectID} item={item} />
       ))
     }
   </ul>
