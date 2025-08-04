944054566dd7bc32c28531863a1bf6200ac3a190
pengibot <pengibot@gmail.com>
2025-08-04 14:33:16 +0100

    Added a fragment to Search to not pollute the DOM with div's

--- src/App.jsx
+++ src/App.jsx
@@ -55,10 +55,10 @@ const App = () => {
 };
 
 const Search = ({ search, onSearch }) => (
-  <div>
+  <>
     <label htmlFor='search'>Search: </label>
     <input id='search' type='text' value={search} onChange={onSearch} />
-  </div>
+  </>
 );
 
 const Item = ( { item }) => (
